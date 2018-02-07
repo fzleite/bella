0a73797321d80e8bf2a76696cf40f267f7bbef36
Fernnado Zimmermann Leite <fzleite@gmail.com>
2018-02-07 17:22:04 -0200

    Carga de dados

---
+++ scripts/app.js
@@ -0,0 +1,181 @@
+ var data = {
+    labels: ["Year 0:Month 1", "Year 0:Month 2", "Year 0:Month 3", "Year 0:Month 4", "Year 0:Month 5", "Year 0:Month 6", "Year 0:Month 7", "Year 0:Month 8", "Year 0:Month 9", "Year 0:Month 11", "Year 0:Month 11", "Year 0:Month 12", "Year 1:Month 1", "Year 1:Month 2", "Year 1:Month 3", "Year 1:Month 4", "Year 1:Month 5", "Year 1:Month 6", "Year 1:Month 7", "Year 1:Month 8", "Year 1:Month 9", "Year 1:Month 11", "Year 1:Month 11", "Year 1:Month 12"],
+    datasets:
+    [
+        {
+            label: '-3SD',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "gray", // The main line color
+            borderWidth: 1,
+            borderCapStyle: 'square',
+            borderDash: [5, 5], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 1,
+            pointRadius: 2,
+            pointHitRadius: 10,
+            data: [43.6, 47.8, 51, 53.5, 55.6, 57.4, 58.9, 60.3, 61.7, 62.9, 64.1, 65.2, 66.3, 67.3, 68.3, 69.3, 70.2, 71.1, 72, 72.8, 73.7, 74.5, 75.2, 76, 76.7]
+        },
+        {
+            label: '-2SD',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "gray", // The main line color
+            borderWidth: 1,
+            borderCapStyle: 'square',
+            borderDash: [5, 5], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 1,
+            pointRadius: 2,
+            pointHitRadius: 10,
+            data: [45.4, 49.8, 53, 55.6, 57.8, 59.6, 61.2, 62.7, 64, 65.3, 66.5, 67.7, 68.9, 70, 71, 72, 73, 74, 74.9, 75.8, 76.7, 77.5, 78.4, 79.2, 80]
+        },
+        {
+            label: '-1SD',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "gray", // The main line color
+            borderWidth: 1,
+            borderCapStyle: 'square',
+            borderDash: [5, 5], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 1,
+            pointRadius: 2,
+            pointHitRadius: 10,
+            data: [47.3, 51.7, 55, 57.7, 59.9, 61.8, 63.5, 65, 66.4, 67.7, 69, 70.3, 71.4, 72.6, 73.7, 74.8, 75.8, 76.8, 77.8, 78.8, 79.7, 80.6, 81.5, 82.3, 83.2]
+        },
+        {
+            label: 'MED',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "gray", // The main line color
+            borderWidth: 1,
+            borderCapStyle: 'square',
+            borderDash: [], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 1,
+            pointRadius: 2,
+            pointHitRadius: 10,
+            data: [49.1, 53.7, 57.1, 59.8, 62.1, 64, 65.7, 67.3, 68.7, 70.1, 71.5, 72.8, 74, 75.2, 76.4, 77.5, 78.6, 79.7, 80.7, 81.7, 82.7, 83.7, 84.6, 85.5, 86.4]
+        },
+        {
+            label: '1SD',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "gray", // The main line color
+            borderWidth: 1,
+            borderCapStyle: 'square',
+            borderDash: [5, 5], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 1,
+            pointRadius: 2,
+            pointHitRadius: 10,
+            data: [51, 55.6, 59.1, 61.9, 64.3, 66.2, 68, 69.6, 71.1, 72.6, 73.9, 75.3, 76.6, 77.8, 79.1, 80.2, 81.4, 82.5, 83.6, 84.7, 85.7, 86.7, 87.7, 88.7, 89.6]
+        },
+        {
+            label: '2SD',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "gray", // The main line color
+            borderWidth: 1,
+            borderCapStyle: 'square',
+            borderDash: [5, 5], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 1,
+            pointRadius: 2,
+            pointHitRadius: 10,
+            data: [52.9, 57.6, 61.1, 64, 66.4, 68.5, 70.3, 71.9, 73.5, 75, 76.4, 77.8, 79.2, 80.5, 81.7, 83, 84.2, 85.4, 86.5, 87.6, 88.7, 89.8, 90.8, 91.9, 92.9]
+        },
+        {
+            label: '3SD',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "gray", // The main line color
+            borderWidth: 1,
+            borderCapStyle: 'square',
+            borderDash: [5, 5], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 1,
+            pointRadius: 2,
+            pointHitRadius: 10,
+            data: [54.7, 59.5, 63.2, 66.1, 68.6, 70.7, 72.5, 74.2, 75.8, 77.4, 78.9, 80.3, 81.7, 83.1, 84.4, 85.7, 87, 88.2, 89.4, 90.6, 91.7, 92.9, 94, 95, 96.1]
+        }
+    ]
+};
+
+var options = {
+    scales: 
+    {
+        yAxes: 
+        [
+            {
+                ticks: 
+                {
+                    beginAtZero:true
+                },
+                scaleLabel: 
+                {
+                    display: true,
+                    labelString: 'LFA Girls',
+                    fontSize: 20 
+                }
+            }
+        ]
+    }
+};
